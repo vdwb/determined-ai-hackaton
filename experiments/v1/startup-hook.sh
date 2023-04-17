1eba31f7b76da3c418a466a0ce904191bf845935
vdwb <130967471+vdwb@users.noreply.github.com>
2023-04-17 19:19:53 +0200

    Add files via upload

---
+++ experiments/v1/startup-hook.sh
@@ -0,0 +1,11 @@
+pip install pandas
+# Python lybrary to read/write excel files
+pip install openpyxl
+# Seaborn is a Python data visualization library based on matplotlib
+pip install seaborn
+# Library Exploratory Data Analysis
+pip install pandas-profiling
+# SHAP (SHapley Additive exPlanations) is a game theoretic approach to explain the output of any machine learning model
+pip install shap
+pip install facets-overview
+pip install geopy
